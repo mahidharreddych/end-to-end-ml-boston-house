25f8ab1decdb6bb7cd77782f7925abd964f9a8e1
mahidharreddych <mahidhar.reddy14@gmail.com>
2022-11-02 21:57:02 +0530

    Adding Final Files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pickle
+import numpy as np
 from flask import Flask, render_template, request
 
 model = pickle.load(open("model.pkl", 'rb'))
+scale = pickle.load(open("scale.pkl", 'rb'))
 
 app = Flask(__name__)
 
@@ -9,9 +11,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/prediction")
+@app.route("/prediction", methods=['POST'])
 def output():
-    return render_template("output.html")
+    values = [float(x) for x in request.form.values()]
+    scaled_values = scale.transform([values])
+    output = model.predict(scaled_values)
+    print(output)
+    return render_template("output.html", output = "Price of the House is : {} " .format(np.abs(output[0])))
 
 
 if __name__ == "__main__":
